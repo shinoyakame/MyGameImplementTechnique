71cc54c7b1fc30e09ebb5082509b8afba09a1f0d
DESKTOP-3A7O5UH\Inukame Lighter <shinoyakame02@gmail.com>
2018-03-26 13:19:35 +0700

    Add Missing Connection Data

--- app3/app3.js
+++ app3/app3.js
@@ -2,6 +2,13 @@ var express = require('express');
 var app = express();
 var mysql = require('mysql');
 
+var connection = mysql.createConnection({
+    host:'localhost', 
+    user:'root', 
+    password:'123456',
+    database:'game1'
+});
+
 connection.connect(function (err) { //async
     if (err) {
         console.log('Error Connecting', err.stack);
